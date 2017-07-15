3c8e92ebd450a7bcada2babc4f02b6b8029c1c0a
Vivi Wen <hw2428@columbia.edu>
2017-07-15 12:30:41 -0400

    add list view, add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Desserts-Native-App
+Desserts recommendation for iOS and Android
+
+## Features/Components
+⋅⋅*  Uses React Native 0.44.0
+⋅⋅*  Swiper view home page
+⋅⋅*  Scroll list view of recommendations
+⋅⋅*  Map view for locations
+⋅⋅*  Share interest point to social network
+
+## Installation
+⋅⋅* Download or clone this project, run `npm install`
+⋅⋅* Run IOS `react-native run-ios`
+⋅⋅* Run Android `react-native run-android`
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -2,29 +2,83 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  ScrollView,
 } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: '#f4f7f9'
   },
-  title: {
+  buttonText: {
     marginBottom: 20,
-    fontSize: 25,
+    fontSize: 18,
     textAlign: 'center',
     color: '#4169E1'
   },
+  rowContainer: {
+    padding: 10
+  },
+  rowTitle: {
+    color: 'orange',
+    fontSize: 16
+  },
+  rowContent: {
+    fontSize: 19
+  }
 })
 
 export default class ListView extends Component {
   render() {
+    const listItem = [  {
+        title: 'Blue Note Jazz Festival',
+        category: 'Concert',
+        latlng: { latitude: 40.7070877, longitude: -74.0057347 },
+        color: 'red',
+        startDate: '',
+        endDate: ''
+      },
+      {
+        title: 'BRIC Celebrate Brooklyn Festival',
+        category: 'Concert',
+        latlng: { latitude: 40.6633672, longitude: -74.0466936 },
+        color: 'red',
+        startDate: '',
+        endDate: ''
+      },
+      {
+        title: 'Free Summer Concerts at MOMA',
+        category: 'Concert',
+        latlng: { latitude: 40.7614327, longitude: -74.0476619 },
+        color: 'red',
+        startDate: '',
+        endDate: ''
+      },
+      {
+        title: 'Shorty Tang',
+        category: 'Food',
+        latlng: { latitude: 40.7404701, longitude: -74.0727109 },
+        color: 'red',
+        startDate: '',
+        endDate: ''
+      }]
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}> List View </Text>
-      </View>
+      <ScrollView style={styles.container}>
+        {
+          listItem.map((item, index) => {
+            return (
+              <View key={index}>
+                <View style={styles.rowContainer}>
+                  <Text style={styles.rowTitle}>{item.title}</Text>
+                  <Text style={styles.rowContainer}>{item.category}</Text>
+                </View>
+              </View>
+            )
+          })
+        }
+
+      </ScrollView>
     )
   }
 }
